42a9292680e96bd36bb902e74b5594f8ee8d609a
Edison Klafke Fillus <edison.fillus@gmail.com>
2020-09-26 16:53:13 -0300

    delete all .js

--- src/loaders/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.initLoaders = void 0;
-const express_1 = require("./express");
-const logger_1 = require("./logger");
-exports.initLoaders = (app) => {
-    express_1.expressLoader(app);
-    logger_1.logger.info('Express loaded');
-};
