a7a671ef3ad6d3ba7773f6053356c4c2135802df
Edison Klafke Fillus <edison.fillus@gmail.com>
2020-09-26 16:53:50 -0300

    configure express to also parse url encoded

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -4,6 +4,7 @@ import {config} from "../config";
 import bodyParser from "body-parser";
 
 export const expressLoader = (app: express.Application) => {
+    app.use(bodyParser.urlencoded({extended:true}));
     app.use(bodyParser.json());
     app.use(config.api.prefix, routes());
 
