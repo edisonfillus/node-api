e796231736ec95c3b2d49c6863bcdccf5b386efe
Edison Klafke Fillus <edison.fillus@gmail.com>
2020-09-26 21:53:15 -0300

    Configure morgan to create access log integrated with Winston

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -3,14 +3,19 @@ import {routes} from '../controllers';
 import {config} from "../config";
 import bodyParser from "body-parser";
 import cors from "cors";
+import {logger} from "./logger";
+import morgan from "morgan";
 
 export const expressLoader = (app: express.Application) => {
+
+    // Enable Access Log
+    app.use(morgan("combined", {stream: {write: message => logger.info(message)}}));
 
     // Enable CORS
     app.use(cors());
 
     // Enable parse of URL Encoded and JSon bodies
-    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
 
     // Configure the API path and include all the routes
