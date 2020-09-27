6e511bfd8ad7753bf408b8ccc36f3ce9bbc84fb1
Edison Klafke Fillus <edison.fillus@gmail.com>
2020-09-26 22:45:20 -0300

    Configured error handler

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,12 +1,13 @@
-import express from 'express';
+import {Application,Request, Response} from 'express';
 import {routes} from '../controllers';
 import {config} from "../config";
 import bodyParser from "body-parser";
 import cors from "cors";
 import {logger} from "./logger";
 import morgan from "morgan";
+import {HttpException} from "../exceptions/HttpException";
 
-export const expressLoader = (app: express.Application) => {
+export const expressLoader = (app: Application) => {
 
     // Enable Access Log
     app.use(morgan("combined", {stream: {write: message => logger.info(message)}}));
@@ -20,5 +21,12 @@ export const expressLoader = (app: express.Application) => {
 
     // Configure the API path and include all the routes
     app.use(config.api.prefix, routes());
+
+    // Error handler
+    app.use((error: HttpException, req: Request, res: Response) => {
+        const status = error.status || 500;
+        const message = error.message || 'Something went wrong';
+        res.status(status).send({status, message});
+    });
 
 }
