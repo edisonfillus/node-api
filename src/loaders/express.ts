dea76120f6a808d0ee8b0623ef2753f0988d8a54
Edison Klafke Fillus <edison.fillus@gmail.com>
2020-09-26 21:31:47 -0300

    include cors configuration

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -2,10 +2,18 @@ import express from 'express';
 import {routes} from '../controllers';
 import {config} from "../config";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 export const expressLoader = (app: express.Application) => {
+
+    // Enable CORS
+    app.use(cors());
+
+    // Enable parse of URL Encoded and JSon bodies
     app.use(bodyParser.urlencoded({extended:true}));
     app.use(bodyParser.json());
+
+    // Configure the API path and include all the routes
     app.use(config.api.prefix, routes());
 
 }
